ca6fd4dd6306a180038fc7b06423ee88664d2380
Róbert Jack <robertalex00@hotmail.com>
2023-04-18 14:26:18 +0000

    Uml mynd byrt á síðunni

--- src/site/Markdown/Markdown.md
+++ src/site/Markdown/Markdown.md
@@ -1,3 +1,3 @@
 # This is our UML diagram
-
+![](Umlmyndlokaverkefni.png)
 
--- src/test/java/vinnsla/KarfaTest.java
+++ src/test/java/vinnsla/KarfaTest.java
@@ -5,16 +5,23 @@ import is.vinnsla.Veitingar;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class KarfaTest {
-    Karfa karfa;
-    Veitingar veitingar;
-
+    int a;
+    int b;
+    // Every class in our project has a GUI component so we do not have the knowledge to test them.
+    // However to show that we are familiar with JUnit testing we made some examples of test.
     @Test
-    public void testKarfaverd() {
-        int a = 100;
-        int heildarverd = 100;
-        assertEquals(heildarverd,a);
-
+    public void testExampleOne() {
+        a = 100;
+        b = 100;
+        assertEquals(a,b);
+    }
+    @Test
+    public void testExampleTwo(){
+        a = 29;
+        b = 31;
+        assertFalse(a == b);
     }
 }
